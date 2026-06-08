1d064664fab52f5fa286a078110137c2b390f9ab
xwx-da1sy <2717565@dundee.ac.uk>
2026-06-09 00:29:09 +0800

    Clean repository structure

---
+++ KNN_algorithm/KNN_clarification.py
@@ -0,0 +1,11 @@
+"""
+KNN_clarification.py
+步骤如下：
+    导包
+    准备数据集（测试集和训练集）
+    创建模型对象
+    模型训练
+    模型预测
+"""
+
+from sklearn.neighbors import KNeighborsClassifier
--- Machine Learning/KNN_algorithm/KNN_clarification.py
+++
@@ -1,4 +0,0 @@
-"""
-KNN_clarification.py
-"""
-
